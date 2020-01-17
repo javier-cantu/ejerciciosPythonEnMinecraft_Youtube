0f28b59e3bedeae4df7cc202395098ae785ec0b5
Javier <pacocantu87@gmail.com>
2020-01-17 09:10:55 -0600

    Create 016_setBlocksCubosTower.py

---
+++ 016_setBlocksCubosTower.py
@@ -0,0 +1,68 @@
+# Ejercicio 016_setBlocksCubosTower
+# Crear una torre con setclocks
+
+#Connect to Minecraft
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+#Necesitamos importar time
+import time
+
+
+# BASE
+##########################################
+# Primero  determinamos la coordenada incial
+x = -20
+y = 4
+z = -20
+
+#Creamos la coordenada final
+x2 = 20
+y2 = 12
+z2 = 20
+
+# Tipo de Bloque 0 es el ID de aire
+blockType = 1
+
+# Construir el bloque usando 2 coordenadas.
+mc.setBlocks(x, y, z, x2, y2, z2, blockType)
+
+
+
+# CENTRO
+##########################################
+# Primero  determinamos la coordenada incial
+x = -10
+y = 13
+z = -10
+
+#Creamos la coordenada final
+x2 = 10
+y2 = 20
+z2 = 10
+
+# Tipo de Bloque 0 es el ID de aire
+blockType = 1
+
+# Construir el bloque usando 2 coordenadas.
+mc.setBlocks(x, y, z, x2, y2, z2, blockType)
+
+
+
+# TORRE
+##########################################
+# Primero  determinamos la coordenada incial
+x = -5
+y = 21
+z = -5
+
+#Creamos la coordenada final
+x2 = 5
+y2 = 55
+z2 = 5
+
+# Tipo de Bloque 0 es el ID de aire
+blockType = 1
+
+# Construir el bloque usando 2 coordenadas.
+mc.setBlocks(x, y, z, x2, y2, z2, blockType)
+

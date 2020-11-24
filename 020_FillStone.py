b8fce0ae0a9865c25e22135af59fa4faf4004591
Javier <pacocantu87@gmail.com>
2020-11-24 17:41:45 -0600

    Update 020_FillStone.py

--- 020_FillStone.py
+++ 020_FillStone.py
@@ -1,5 +1,7 @@
 # Ejercicio 020_FillStone
-# Rellenar o convertir en piedra un bloque con centro en el origen
+# Continuacion al ejercicio 019_ClearOriginCube.py
+# Rellenar o convertir en piedra el espacio que se creó
+# Hasta en nivel del agua del mundo de Minecraft
 
 #Connect to Minecraft
 from mcpi.minecraft import Minecraft
@@ -26,5 +28,6 @@ z2 = 50
 # Tipo de Bloque 1 es el ID de piedra
 blockType = 1
 
-# Construir el bloque usando 2 coordenadas.
+# Construir el bloque usando 2 coordenadas y un blockID
+# usamos todas las variables creadas.
 mc.setBlocks(x, y, z, x2, y2, z2, blockType)

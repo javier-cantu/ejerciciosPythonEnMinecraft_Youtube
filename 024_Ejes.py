d6fc91f5d5d27c2cecfa92c8ef2c0eccb9888542
Javier <pacocantu87@gmail.com>
2021-03-03 17:53:13 -0600

    Update 024_Ejes.py

--- 024_Ejes.py
+++ 024_Ejes.py
@@ -1,4 +1,4 @@
-# Ejercicio 24_
+# Ejercicio 24_Ejes
 # Se crean los ejes para mostrar direcciones
 
 #Connect to Minecraft
@@ -13,20 +13,20 @@ time.sleep(5)
 blockType = 251,5
 # Eje Y
 time.sleep(2)
-mc.setBlocks(0, 4, 0, 0, 24, 0, blockType)
+mc.setBlocks(0, 4, 0, 0, 104, 0, blockType)
 
 # Red X
 blockType = 251,14
 # Eje X
 time.sleep(2)
-mc.setBlocks(1, 4, 0, 25, 4, 0, blockType)
+mc.setBlocks(1, 4, 0, 105, 4, 0, blockType)
 
 # Blue Z
 blockType = 251,11
 # Blue Z
 time.sleep(2)
-mc.setBlocks(0, 4, 1, 0, 4, 25, blockType)
+mc.setBlocks(0, 4, 1, 0, 4, 105, blockType)
 
 time.sleep(2)
-#Bloque 1, 4, 1
+#Bloque 1, 4, 1 
 mc.setBlock(1, 4, 1, 251,4)

970f4291f2f36cce1a728f54e12788ce7dadf409
Javier <pacocantu87@gmail.com>
2021-03-03 18:24:19 -0600

    Update 026_Intro2_BlockOrientation.py

--- 026_Intro2_BlockOrientation.py
+++ 026_Intro2_BlockOrientation.py
@@ -47,26 +47,3 @@ mc.setBlock(x, y, z+26, blockType,12)
 mc.setBlock(x, y, z+28, blockType,13) 
 mc.setBlock(x, y, z+30, blockType,14)
 mc.setBlock(x, y, z+30, blockType,15)
-
-
-# Se determina cual block ID se usara
-blockType = 63
-# En x +2
-x = x + 2
-# Se crean los bloques con data
-mc.setBlock(x, y, z+2, blockType,0) 
-mc.setBlock(x, y, z+4, blockType,1) 
-mc.setBlock(x, y, z+6, blockType,2)
-mc.setBlock(x, y, z+8, blockType,3) 
-mc.setBlock(x, y, z+10, blockType,4) 
-mc.setBlock(x, y, z+12, blockType,5)
-mc.setBlock(x, y, z+14, blockType,6) 
-mc.setBlock(x, y, z+16, blockType,7) 
-mc.setBlock(x, y, z+18, blockType,8)
-mc.setBlock(x, y, z+20, blockType,9) 
-mc.setBlock(x, y, z+22, blockType,10) 
-mc.setBlock(x, y, z+24, blockType,11)
-mc.setBlock(x, y, z+26, blockType,12) 
-mc.setBlock(x, y, z+28, blockType,13) 
-mc.setBlock(x, y, z+30, blockType,14)
-mc.setBlock(x, y, z+30, blockType,15)

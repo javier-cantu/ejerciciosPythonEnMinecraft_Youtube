e5893ae5587705a987368d93ea889a3d5cd5fdff
Javier <pacocantu87@gmail.com>
2021-12-01 03:45:52 -0600

    Update 033_getTilePos_intro.py

--- 033_getTilePos_intro.py
+++ 033_getTilePos_intro.py
@@ -1,28 +1,32 @@
 # Ejercicio 033_getTilePos_intro getTilePos obtiene la posicion del jugador y la manda al chat
 # Video en Youtube: 
 
-
 # Connect to Minecraft
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
+import time
 
-# Con "mc.player.getTilePos()" se obtienen los 3 valores de x y z de la posición del jugador. 
-# Esos 3 valores se asignan a la variable "pos"
+# Con "mc.player.getTilePos()" se obtiene la posicion del "tile" o block donde se encuentra el jugador.  
+# Esta funcion regresa la posición como un vector
+# La "tile position" se guarda como un integer que representa la posición exacta de un block o "tile" en el mundo. 
+# Se puede separar este vector en valores individuales para cada eje, x, y, z. 
+# https://pimylifeup.com/minecraft-pi-edition-api-reference/
 
-
-Gets the position of the “tile” that the player is currently on.
-
-This function returns the position as a vector. The tile position is stored as an integer as it represents the blocks exact position within the game world.
-
-Like the .getPos() function, you can also use Python’s unpack ability to unpack this vector back to the individual x, y, and z values.
-
+# Se le asigna el valor del vector a la variable "pos"
 pos = mc.player.getTilePos()
 
-# Luego asignamos cada valor a una variable diferente
-x = pos.x
-y = pos.y
-z = pos.z
+# Se manda al chat el valor de "pos"
+mc.postToChat(pos)
+# Pausa de 1 segundo
+time.sleep(1)
 
+# Luego separamos el vector en 3 valores
+# Con pos.x se accesa al componente X del vector
+# hacemos lo mismo con cada eje y se asiga cada valor a una variable diferente
+# xi, yi, zi, representan los valores iniciales
+xi = pos.x
+yi = pos.y
+zi = pos.z
 
-# Convertir en RANDOM el bloque abajo
-mc.postToChat("Player Tile Position = " + " X = " + str(x) + "  Y = " +str(y) + "  Z = " + str(z))
+# Se manda al chat la posicion del blocke donde estan los pies del jugador. 
+mc.postToChat("La posición del jugador es: = " + " X = " + str(x) + "  Y = " +str(y) + "  Z = " + str(z))

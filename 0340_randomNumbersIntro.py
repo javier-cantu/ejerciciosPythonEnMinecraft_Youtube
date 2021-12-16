f3f10983149667f7b9ed5b31db5520514debc782
Javier <pacocantu87@gmail.com>
2021-12-16 09:44:11 -0600

    Create 0340_randomNumbersIntro.py

---
+++ 0340_randomNumbersIntro.py
@@ -0,0 +1,26 @@
+lines (34 sloc) 1.21 KB
+# Ejercicio 039_nestedNestedLoop
+# Un loop dentro de un loop dentro de otro loop
+# Video en Youtube: 
+
+# Nos conectamos
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+# Modulo time
+import time
+
+# Ahora importamos el modulo random
+# que nos da acceso a los numeros aleatorios
+import random
+
+time.sleep(3)
+
+for i in range(10):
+    # Con la funcion "randomint()" se regresa un numero aleatorio
+    # de tipo int, entre los valores que se pongan dentro de los ()
+    num_random = random.randint(1, 100)
+    mc.postToChat("El numero random es: " + str(num_random))
+    print(num_random)
+    time.sleep(1)
+
+mc.postToChat("Fin de los numeros aleatorios")
